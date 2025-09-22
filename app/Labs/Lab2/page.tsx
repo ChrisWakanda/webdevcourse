b3aa6ddc32115e6058afee6a323f0e4616442546
Aryan Raveshia <aryanraveshia@Aryans-MacBook-Pro.local>
2025-09-22 13:15:21 -0400

    version control related change

--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -1,12 +1,10 @@
+import "./index.css";
 export default function Lab2(){
     return (
         <div>
             <h1>
-                Lab 2
+                Lab 2 - Cascanding Style Sheets (CSS) Basics
             </h1>
-            <h3>
-                CSS Basics
-            </h3>
         </div>
     )
 }
