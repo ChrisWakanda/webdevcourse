5bd8e7df04d00a0b64829f07e342bf3193cd2637
Aryan Raveshia <aryanraveshia@Aryans-MacBook-Pro.local>
2025-10-01 01:32:41 -0400

    minor change: took out '' in Lab2/page.tsx to resolve vercel deployment conflict

--- app/Labs/Lab2/page.tsx
+++ app/Labs/Lab2/page.tsx
@@ -25,10 +25,10 @@ export default function Lab2(){
                 Lab 2 - Cascading Style Sheets (CSS) Basics
             </h2>
             <h3>
-                Styling with the 'STYLE' attribute
+                Styling with the STYLE attribute
             </h3>
             <p id="para-1" style={{color: "darkblue", backgroundColor: "lightblue"}}>
-                Foreground color - Font color. Background color - color of the background of the tag that you chose. Ex: here it is p, which is paragraph. The entire size of the paragraph on screen will then be the color chosen for 'backgroundColor'
+                Foreground color - Font color. Background color - color of the background of the tag that you chose. Ex: here it is p, which is paragraph. The entire size of the paragraph on screen will then be the color chosen for backgroundColor
             </p>
             <h3>
                 Styling with ID Selectors:
@@ -37,13 +37,13 @@ export default function Lab2(){
                 A. 
             </h4>
             <p id="para-2">
-                This para has been made via css id selectors (white on red). There are multiple classes that one can combine, some from tailwind some from bootstrap, some from react icons etc etc which all have different classes and names - this results in combining multiple classes from different sources as listed below which makes us confused as to which classes are ours and which are actually inherent classes of these technologies. Hence, here, the professor has pre-pended each className with 'wd' to make sure that all classes that defined by him are easily recognizable. 
+                This para has been made via css id selectors (white on red). There are multiple classes that one can combine, some from tailwind some from bootstrap, some from react icons etc etc which all have different classes and names - this results in combining multiple classes from different sources as listed below which makes us confused as to which classes are ours and which are actually inherent classes of these technologies. Hence, here, the professor has pre-pended each className with wd to make sure that all classes that defined by him are easily recognizable. 
             </p>
             <h4>
                 B. 
             </h4>
             <p id="para-3">
-                This para has been made via css id selectors (black on yellow). There are multiple classes that one can combine, some from tailwind some from bootstrap, some from react icons etc etc which all have different classes and names - this results in combining multiple classes from different sources as listed below which makes us confused as to which classes are ours and which are actually inherent classes of these technologies. Hence, here, the professor has pre-pended each className with 'wd' to make sure that all classes that defined by him are easily recognizable. 
+                This para has been made via css id selectors (black on yellow). There are multiple classes that one can combine, some from tailwind some from bootstrap, some from react icons etc etc which all have different classes and names - this results in combining multiple classes from different sources as listed below which makes us confused as to which classes are ours and which are actually inherent classes of these technologies. Hence, here, the professor has pre-pended each className with wd to make sure that all classes that defined by him are easily recognizable. 
             </p>
             <h3>
                 Styling with Class Selectors:
@@ -52,13 +52,13 @@ export default function Lab2(){
                 A.
             </h4>
             <p className="wd-fg-color-blue wd-bg-color-yellow">
-                This para is made via class selectors (blue on yellow para) There are multiple classes that one can combine, some from tailwind some from bootstrap, some from react icons etc etc which all have different classes and names - this results in combining multiple classes from different sources as listed below which makes us confused as to which classes are ours and which are actually inherent classes of these technologies. Hence, here, the professor has pre-pended each className with 'wd' to make sure that all classes that defined by him are easily recognizable. 
+                This para is made via class selectors (blue on yellow para) There are multiple classes that one can combine, some from tailwind some from bootstrap, some from react icons etc etc which all have different classes and names - this results in combining multiple classes from different sources as listed below which makes us confused as to which classes are ours and which are actually inherent classes of these technologies. Hence, here, the professor has pre-pended each className with wd to make sure that all classes that defined by him are easily recognizable. 
             </p>
             <h4>
                 B.
             </h4>
             <h3 className="wd-fg-color-blue wd-bg-color-yellow">
-                This heading is made via class selectors (blue on yellow heading) There are multiple classes that one can combine, some from tailwind some from bootstrap, some from react icons etc etc which all have different classes and names - this results in combining multiple classes from different sources as listed below which makes us confused as to which classes are ours and which are actually inherent classes of these technologies. Hence, here, the professor has pre-pended each className with 'wd' to make sure that all classes that defined by him are easily recognizable. 
+                This heading is made via class selectors (blue on yellow heading) There are multiple classes that one can combine, some from tailwind some from bootstrap, some from react icons etc etc which all have different classes and names - this results in combining multiple classes from different sources as listed below which makes us confused as to which classes are ours and which are actually inherent classes of these technologies. Hence, here, the professor has pre-pended each className with wd to make sure that all classes that defined by him are easily recognizable. 
             </h3>
             <div id="wd-css-document-structure">
                 <div>
