dde8e523d487a0238d2f4fcca4e6be672de93ad8
Aryan Raveshia <aryanraveshia@Aryans-MacBook-Pro.local>
2025-10-12 15:22:16 -0400

    Lab3 done. Kambaz work remaining

--- app/Labs/Lab3/page.tsx
+++ app/Labs/Lab3/page.tsx
@@ -1,13 +1,19 @@
+import Add from "./Add";
+import Highlight from "./Highlight";
 import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
 import ArrayIndexAndLength from "./ArrayIndexAndLength";
 import ArrowFunctions from "./ArrowFunctions";
 import BooleanVariables from "./BooleanVariables";
+import Classes from "./Classes";
 import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
 import ConditionalOutputInline from "./ConditionalOutputInline";
+import Destructing from "./Destructing";
+import DestructingImports from "./DestructingImports";
 import FilterFunction from "./FilterFunction";
 import FindFunction from "./FindFunction";
 import FindIndex from "./FindIndex";
 import ForLoops from "./ForLoops";
+import FunctionDestructing from "./FunctionDestructing";
 import House from "./House";
 import IfElse from "./IfElse";
 import ImpliedReturns from "./ImpliedReturns";
@@ -15,11 +21,15 @@ import JsonStringify from "./JsonStringify";
 import LegacyFunctions from "./LegacyFunctions";
 import MapFunction from "./MapFunction";
 import SimpleArrays from "./SimpleArrays";
+import Spreading from "./Spreader";
+import Square from "./Square";
+import Styles from "./Styles";
 import TemplateLiterals from "./TemplateLiterals";
 import TernaryOperator from "./TernaryOperator";
 import TodoList from "./todos/TodoList";
 import VariablesAndConstants from "./VariablesAndConstants";
 import VariableTypes from "./VariableTypes";
+import PathParameters from "./PathParameters";
 
 export default function Lab3() {
   return (
@@ -49,5 +59,18 @@ export default function Lab3() {
       <JsonStringify/>
       <House/>
       <TodoList/>
+      <Spreading/>
+      <Destructing/>
+      <FunctionDestructing/>
+      <DestructingImports/>
+      <Classes/>
+      <Styles/><hr/>
+      <Add a={30} b={45}/>
+      <h4>Square of 10</h4><Square>10</Square><hr />
+      <h4>Usage of Component Parameterization using Children Nodes as Strings:</h4><Highlight>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipitratione eaque illo minus cum, saepe totam
+        vel nihil repellat nemo explicabo excepturi consectetur. Modi omnis minus sequi maiores, provident voluptates.
+     </Highlight><hr/>
+     <PathParameters/><hr/>
     </div>
 );}
