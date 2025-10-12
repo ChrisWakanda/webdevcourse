b731ab1c08417728aba178efa61abfc5f1291039
Aryan Raveshia <aryanraveshia@Aryans-MacBook-Pro.local>
2025-10-11 23:11:16 -0400

    Lab 3 1/2 done

--- app/Labs/Lab3/page.tsx
+++ app/Labs/Lab3/page.tsx
@@ -1,12 +1,53 @@
-export default function Lab3(){
-    return (
-        <div>
-            <h1>
-                Lab 3
-            </h1>
-            <h3>
-                Javascript Fundamentals
-            </h3>
-        </div>
-    )
-}
+import AddingAndRemovingToFromArrays from "./AddingAndRemovingToFromArrays";
+import ArrayIndexAndLength from "./ArrayIndexAndLength";
+import ArrowFunctions from "./ArrowFunctions";
+import BooleanVariables from "./BooleanVariables";
+import ConditionalOutputIfElse from "./ConditionalOutputIfElse";
+import ConditionalOutputInline from "./ConditionalOutputInline";
+import FilterFunction from "./FilterFunction";
+import FindFunction from "./FindFunction";
+import FindIndex from "./FindIndex";
+import ForLoops from "./ForLoops";
+import House from "./House";
+import IfElse from "./IfElse";
+import ImpliedReturns from "./ImpliedReturns";
+import JsonStringify from "./JsonStringify";
+import LegacyFunctions from "./LegacyFunctions";
+import MapFunction from "./MapFunction";
+import SimpleArrays from "./SimpleArrays";
+import TemplateLiterals from "./TemplateLiterals";
+import TernaryOperator from "./TernaryOperator";
+import TodoList from "./todos/TodoList";
+import VariablesAndConstants from "./VariablesAndConstants";
+import VariableTypes from "./VariableTypes";
+
+export default function Lab3() {
+  return (
+    <div id="wd-lab3">
+      <h1>Lab 3</h1><br/>
+      <h3>JavaScript Fundamentals</h3><br/>
+      <hr/>
+      <VariablesAndConstants/>
+      <VariableTypes/>
+      <BooleanVariables/>
+      <IfElse/>
+      <TernaryOperator/>
+      <ConditionalOutputIfElse/>
+      <ConditionalOutputInline/>
+      <LegacyFunctions/>
+      <ArrowFunctions/>
+      <ImpliedReturns/>
+      <TemplateLiterals/>
+      <SimpleArrays/>
+      <ArrayIndexAndLength/>
+      <AddingAndRemovingToFromArrays/>
+      <ForLoops/>
+      <MapFunction/>
+      <FindFunction/>
+      <FindIndex/>
+      <FilterFunction/>
+      <JsonStringify/>
+      <House/>
+      <TodoList/>
+    </div>
+);}
