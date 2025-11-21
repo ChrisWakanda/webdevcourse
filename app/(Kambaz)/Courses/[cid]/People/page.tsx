f08a3df7b6dd5e6885188b02d4adf7ec73e279f5
Aryan Raveshia <aryanraveshia@Aryans-MacBook-Pro.local>
2025-11-21 18:28:30 -0500

    fixed peopletable page.tsx code

--- app/(Kambaz)/Courses/[cid]/People/page.tsx
+++ app/(Kambaz)/Courses/[cid]/People/page.tsx
@@ -1,12 +1,41 @@
+// import PeopleTable from "./Table/page";
+
+// export default function People() {
+//     return (
+//         <div>
+//             <h1>
+//                 People
+//             </h1>
+//             <PeopleTable />
+//         </div>
+//     )
+// }
+
+
+"use client";
+import { useState, useEffect } from "react";
 import PeopleTable from "./Table/page";
+import * as client from "../../../Account/client";
+import { useParams } from "next/navigation";
 
 export default function People() {
-    return (
-        <div>
-            <h1>
-                People
-            </h1>
-            <PeopleTable/>
-        </div>
-    )
+  const [users, setUsers] = useState<any[]>([]);
+  const { cid } = useParams();
+
+  const fetchUsers = async () => {
+    const users = await client.findAllUsers();
+    setUsers(users);
+  };
+
+  useEffect(() => {
+    fetchUsers();
+  }, []);
+
+  return (
+    <div>
+      <h1>People</h1>
+      {/* We pass the data down here so PeopleTable stops complaining */}
+      <PeopleTable users={users} fetchUsers={fetchUsers} />
+    </div>
+  );
 }
