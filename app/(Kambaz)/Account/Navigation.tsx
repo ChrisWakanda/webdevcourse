2664b1511364ffd0e544780a09ad1f0bba8784fa
Aryan Raveshia <aryanraveshia@Aryans-MacBook-Pro.local>
2025-11-21 16:52:46 -0500

    initial commit with changes for a6

--- app/(Kambaz)/Account/Navigation.tsx
+++ app/(Kambaz)/Account/Navigation.tsx
@@ -1,30 +1,45 @@
+// "use client";
 // import Link from "next/link";
+// import { usePathname } from "next/navigation";
+// import { Nav, NavItem, NavLink } from "react-bootstrap";
+// import { useSelector } from "react-redux";
 // export default function AccountNavigation() {
-//     return (
-//         <div id="wd-account-navigation" className="wd list-group fs-5 rounded-0">
-//             <Link href="/Account/Signin" id="wd-course-home-link" className="list-group-item active border-0">Sign In</Link>
-//             <Link href="/Account/Signup" id="wd-course-modules-link" className="list-group-item text-danger border-0">Sign up
-//                 </Link>
-//             <Link href="/Account/Profile" id="wd-course-piazza-link" className="list-group-item text-danger border-0">Profile</Link>
-//       </div>
-//     )
-// }
+//     // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//   const { currentUser } = useSelector((state: any) => state.accountReducer);
+//   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
+//  const pathname = usePathname();
+//  return (
+//    <Nav variant="pills">
+//      {links.map((link) => (
+//        <NavItem key={link}>
+//          <NavLink as={Link} href={link} active={pathname.endsWith(link.toLowerCase())}>
+//            {link} </NavLink> </NavItem>
+//      ))}
+//    </Nav>
+// );}
+
 
 "use client";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
 import { Nav, NavItem, NavLink } from "react-bootstrap";
 import { useSelector } from "react-redux";
+
 export default function AccountNavigation() {
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const { currentUser } = useSelector((state: any) => state.accountReducer);
   const links = currentUser ? ["Profile"] : ["Signin", "Signup"];
- const pathname = usePathname();
+  const pathname = usePathname();
+
+  if (currentUser && currentUser.role === "ADMIN") {
+    links.push("Users");
+  }
+
  return (
    <Nav variant="pills">
      {links.map((link) => (
        <NavItem key={link}>
-         <NavLink as={Link} href={link} active={pathname.endsWith(link.toLowerCase())}>
+         <NavLink as={Link} href={link} active={pathname.endsWith(link) || pathname.endsWith(link.toLowerCase())}>
            {link} </NavLink> </NavItem>
      ))}
    </Nav>
--- app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
+++ app/(Kambaz)/Courses/[cid]/People/Table/page.tsx
@@ -1,16 +1,136 @@
+// "use client";
+// import { Table } from "react-bootstrap";
+// import { FaUserCircle } from "react-icons/fa";
+// import { useParams } from "next/navigation";
+// import * as db from "../.../../../../../Database";
+// export default function PeopleTable() {
+
+//   const { cid } = useParams();
+//   const users = db.user;
+//   const enrollments = db.enrollments;
+
+//   return (
+//     <div id="wd-people-table">
+//       <Table striped responsive>
+//         <thead>
+//           <tr><th>Name</th><th>Login ID</th><th>Section</th><th>Role</th><th>Last Activity</th><th>Total Activity</th></tr>
+//         </thead>
+//         <tbody>
+
+//           {users
+//             .filter((usr) =>
+//               // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//               enrollments.some((enrollment: { user: any; course: any; }) => enrollment.user === usr._id && enrollment.course === cid)
+//             )
+//             // eslint-disable-next-line @typescript-eslint/no-explicit-any
+//             .map((user: any) => (
+//               <tr key={user._id}>
+//                 <td className="wd-full-name text-nowrap">
+//                   <FaUserCircle className="me-2 fs-1 text-secondary" />
+//                   <span className="wd-first-name">{user.firstName}</span>
+//                   <span className="wd-last-name">{user.lastName}</span>
+//                 </td>
+//                 <td className="wd-login-id">{user.loginId}</td>
+//                 <td className="wd-section">{user.section}</td>
+//                 <td className="wd-role">{user.role}</td>
+//                 <td className="wd-last-activity">{user.lastActivity}</td>
+//                 <td className="wd-total-activity">{user.totalActivity}</td>
+//               </tr>
+//             ))}
+
+
+
+//           {/* <tr>
+//             <td className="wd-full-name text-nowrap">
+//               <FaUserCircle className="me-2 fs-1 text-secondary" />
+//               <span className="wd-first-name">Tony</span>{" "}
+//               <span className="wd-last-name">Stark</span></td>
+//             <td className="wd-login-id">001234561S</td>
+//             <td className="wd-section">S101</td>
+//             <td className="wd-role">STUDENT</td>
+//             <td className="wd-last-activity">2020-10-01</td>
+//             <td className="wd-total-activity">10:21:32</td>
+//           </tr>
+//           <tr>
+//             <td className="wd-full-name text-nowrap">
+//               <FaUserCircle className="me-2 fs-1 text-secondary" />
+//               <span className="wd-first-name">Bruce</span>{" "}
+//               <span className="wd-last-name">Wayne</span></td>
+//             <td className="wd-login-id">001234561S</td>
+//             <td className="wd-section">S102</td>
+//             <td className="wd-role">STUDENT</td>
+//             <td className="wd-last-activity">2020-09-01</td>
+//             <td className="wd-total-activity">13:29:12</td>
+//           </tr>
+//           <tr>
+//             <td className="wd-full-name text-nowrap">
+//               <FaUserCircle className="me-2 fs-1 text-secondary" />
+//               <span className="wd-first-name">Steve</span>{" "}
+//               <span className="wd-last-name">Rogers</span></td>
+//             <td className="wd-login-id">001298561S</td>
+//             <td className="wd-section">S103</td>
+//             <td className="wd-role">STUDENT</td>
+//             <td className="wd-last-activity">2020-09-028</td>
+//             <td className="wd-total-activity">05:11:54</td>
+//           </tr>
+//           <tr>
+//             <td className="wd-full-name text-nowrap">
+//               <FaUserCircle className="me-2 fs-1 text-secondary" />
+//               <span className="wd-first-name">Natasha</span>{" "}
+//               <span className="wd-last-name">Romanoff</span></td>
+//             <td className="wd-login-id">001234523S</td>
+//             <td className="wd-section">S101</td>
+//             <td className="wd-role">STUDENT</td>
+//             <td className="wd-last-activity">2020-09-09</td>
+//             <td className="wd-total-activity">17:34:51</td>
+//           </tr>
+//           <tr>
+//             <td className="wd-full-name text-nowrap">
+//               <FaUserCircle className="me-2 fs-1 text-secondary" />
+//               <span className="wd-first-name">Aryan</span>{" "}
+//               <span className="wd-last-name">Raveshia</span></td>
+//             <td className="wd-login-id">001235453S</td>
+//             <td className="wd-section">S101</td>
+//             <td className="wd-role">STUDENT</td>
+//             <td className="wd-last-activity">2020-09-09</td>
+//             <td className="wd-total-activity">11:11:11</td>
+//           </tr>
+//           Add at least 3 more users such as Bruce Wayne, Steve Rogers, and Natasha Romanoff */}
+//         </tbody>
+//       </Table>
+//     </div>);
+// }
+
+
 "use client";
 import { Table } from "react-bootstrap";
 import { FaUserCircle } from "react-icons/fa";
-import { useParams } from "next/navigation";
-import * as db from "../.../../../../../Database";
-export default function PeopleTable() {
+//import { useParams } from "next/navigation";
+//import * as db from "../.../../../../../Database";
+import * as client from "../../../../Account/client";
+import PeopleDetails from "../PeopleDetails/page";
+import Link from "next/link";
+import { useState } from "react";
 
-  const { cid } = useParams();
-  const users = db.user;
-  const enrollments = db.enrollments;
+export default function PeopleTable({ users = [], fetchUsers }: { users?: any[]; fetchUsers: () => void; }) {
+
+  //const { cid } = useParams();
+  //const users = db.user;
+  //const enrollments = db.enrollments;
+
+  const [showDetails, setShowDetails] = useState(false);
+  const [showUserId, setShowUserId] = useState<string | null>(null);
 
   return (
     <div id="wd-people-table">
+      {showDetails && (
+        <PeopleDetails
+          uid={showUserId}
+          onClose={() => setShowDetails(false)} // Just close here
+          fetchUsers={fetchUsers}
+           />
+      )}
+
       <Table striped responsive>
         <thead>
           <tr><th>Name</th><th>Login ID</th><th>Section</th><th>Role</th><th>Last Activity</th><th>Total Activity</th></tr>
@@ -18,17 +138,23 @@ export default function PeopleTable() {
         <tbody>
 
           {users
-            .filter((usr) =>
-              // eslint-disable-next-line @typescript-eslint/no-explicit-any
-              enrollments.some((enrollment: { user: any; course: any; }) => enrollment.user === usr._id && enrollment.course === cid)
-            )
+            // .filter((usr) =>
+            //   // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            //   enrollments.some((enrollment: { user: any; course: any; }) => enrollment.user === usr._id && enrollment.course === cid)
+            // )
             // eslint-disable-next-line @typescript-eslint/no-explicit-any
             .map((user: any) => (
               <tr key={user._id}>
                 <td className="wd-full-name text-nowrap">
-                  <FaUserCircle className="me-2 fs-1 text-secondary" />
-                  <span className="wd-first-name">{user.firstName}</span>
-                  <span className="wd-last-name">{user.lastName}</span>
+                  <span className="text-decoration-none"
+                    onClick={() => {
+                      setShowDetails(true);
+                      setShowUserId(user._id);
+                    }} >
+                    <FaUserCircle className="me-2 fs-1 text-secondary" />
+                    <span className="wd-first-name">{user.firstName}</span>
+                    <span className="wd-last-name">{user.lastName}</span>
+                  </span>
                 </td>
                 <td className="wd-login-id">{user.loginId}</td>
                 <td className="wd-section">{user.section}</td>
@@ -37,65 +163,6 @@ export default function PeopleTable() {
                 <td className="wd-total-activity">{user.totalActivity}</td>
               </tr>
             ))}
-
-
-
-          {/* <tr>
-            <td className="wd-full-name text-nowrap">
-              <FaUserCircle className="me-2 fs-1 text-secondary" />
-              <span className="wd-first-name">Tony</span>{" "}
-              <span className="wd-last-name">Stark</span></td>
-            <td className="wd-login-id">001234561S</td>
-            <td className="wd-section">S101</td>
-            <td className="wd-role">STUDENT</td>
-            <td className="wd-last-activity">2020-10-01</td>
-            <td className="wd-total-activity">10:21:32</td>
-          </tr>
-          <tr>
-            <td className="wd-full-name text-nowrap">
-              <FaUserCircle className="me-2 fs-1 text-secondary" />
-              <span className="wd-first-name">Bruce</span>{" "}
-              <span className="wd-last-name">Wayne</span></td>
-            <td className="wd-login-id">001234561S</td>
-            <td className="wd-section">S102</td>
-            <td className="wd-role">STUDENT</td>
-            <td className="wd-last-activity">2020-09-01</td>
-            <td className="wd-total-activity">13:29:12</td>
-          </tr>
-          <tr>
-            <td className="wd-full-name text-nowrap">
-              <FaUserCircle className="me-2 fs-1 text-secondary" />
-              <span className="wd-first-name">Steve</span>{" "}
-              <span className="wd-last-name">Rogers</span></td>
-            <td className="wd-login-id">001298561S</td>
-            <td className="wd-section">S103</td>
-            <td className="wd-role">STUDENT</td>
-            <td className="wd-last-activity">2020-09-028</td>
-            <td className="wd-total-activity">05:11:54</td>
-          </tr>
-          <tr>
-            <td className="wd-full-name text-nowrap">
-              <FaUserCircle className="me-2 fs-1 text-secondary" />
-              <span className="wd-first-name">Natasha</span>{" "}
-              <span className="wd-last-name">Romanoff</span></td>
-            <td className="wd-login-id">001234523S</td>
-            <td className="wd-section">S101</td>
-            <td className="wd-role">STUDENT</td>
-            <td className="wd-last-activity">2020-09-09</td>
-            <td className="wd-total-activity">17:34:51</td>
-          </tr>
-          <tr>
-            <td className="wd-full-name text-nowrap">
-              <FaUserCircle className="me-2 fs-1 text-secondary" />
-              <span className="wd-first-name">Aryan</span>{" "}
-              <span className="wd-last-name">Raveshia</span></td>
-            <td className="wd-login-id">001235453S</td>
-            <td className="wd-section">S101</td>
-            <td className="wd-role">STUDENT</td>
-            <td className="wd-last-activity">2020-09-09</td>
-            <td className="wd-total-activity">11:11:11</td>
-          </tr>
-          Add at least 3 more users such as Bruce Wayne, Steve Rogers, and Natasha Romanoff */}
         </tbody>
       </Table>
     </div>);
